e8c0eee8794272a958b1c8140fdd781eb7737ddf
Maksym Pohorielov <asassindark57@gmail.com>
2026-08-01 21:15:47 +0300

    fix first task

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,4 +1,5 @@
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const gallery = document.querySelector(".gallery")
 const lightbox = new SimpleLightbox('.gallery a', {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 import { getImages } from "./js/pixabay-api";
 import { createGallery, clearGallery, showLoader, hideLoader } from "./js/render-functions";
 import iziToast from "izitoast";
